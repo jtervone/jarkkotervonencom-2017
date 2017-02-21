022d70ed8d9750abb9049c8b7ec9fa71c74ad6f5
Jarkko Tervonen <jarkko@splatweb.net>
2017-02-21 21:54:16 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -f "deploy.lock" ]; then
+  bundle install
+  jekyll build
+  rm -rf /var/www/jarkkotervonencom
+  mkdir /var/www/jarkkotervonencom
+  mv _site/* /var/www/jarkkotervonencom/
+else
+  echo "Deploy file exists"!
+  exit
+fi
