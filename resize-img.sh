757f95f60e586a5bdd9a855f82f74987331eecb5
Jarkko Tervonen <jarkko.tervonen@gmail.com>
2018-09-27 13:33:07 +0300

    Images resized

--- resize-img.sh
+++ resize-img.sh
@@ -7,6 +7,6 @@ for FILE in $SOURCE
 do
   if [ ! -f ${TARGET}${FILE##*/} ]; then
     echo "Processing ${FILE##*/} file..."
-    convert -resize "600x600>" ${FILE} ${TARGET}${FILE##*/}
+    convert -resize "1024x1024>" ${FILE} ${TARGET}${FILE##*/}
   fi
 done
