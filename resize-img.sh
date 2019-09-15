529cc6fc04b5503217a27760583c4729c8f0fb3a
Jarkko Tervonen <jarkko.tervonen@gmail.com>
2019-09-15 17:02:08 +0300

    WebP versions added

--- resize-img.sh
+++ resize-img.sh
@@ -3,10 +3,17 @@
 SOURCE=assets/img/posts/orig/*
 TARGET=assets/img/posts/
 
+echo "===== Resize images ========================="
 for FILE in $SOURCE
 do
   if [ ! -f ${TARGET}${FILE##*/} ]; then
     echo "Processing ${FILE##*/} file..."
     convert -resize "1024x1024>" ${FILE} ${TARGET}${FILE##*/}
   fi
+done
+
+echo "===== Converting images into WebP .webp ====="
+for x in `find assets/img/posts -type f \( -iname \*.jpeg -o -iname \*.jpg -o -iname \*.JPG -o -iname \*.png \)`
+  do cwebp -quiet -q 80 ${x} -o ${x}.webp;
+  echo "Converted $x to ${x}.webp"
 done
