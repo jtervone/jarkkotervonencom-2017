94c9c2a3d21506354891f5325143de677d9ea276
Jarkko Tervonen <jarkko.tervonen@gmail.com>
2019-10-13 13:05:08 +0300

    Pages fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,19 +2,18 @@
   <Layout class="home">
     <div v-for="{ node } in $page.allBlogPost.edges" :key="node._id" class="article">
       <section class="header">
-        <router-link :to="node.path">
+        <g-link :to="node.path">
           <h2 v-html="node.title"/>
-        </router-link>
+        </g-link>
         <p class="description">
           <span v-html="node.date"/>
         </p>
-        <router-link :to="node.path">
+        <g-link v-if="node.image" :to="node.path">
           <g-image v-if="node.image" :src="node.image" :alt="node.imageText" />
-        </router-link>
+        </g-link>
       </section>
       <section>
         <p v-html="node.content.split('\n')[0]" />
-        <p><router-link :to="node.path">Lue koko kirjoitus</router-link></p>
       </section>
     </div>
     <section>
