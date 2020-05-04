41e420fac356d25ea9a789e1ea9eeff04d5e9c2b
Jarkko Tervonen <jarkko.tervonen@gmail.com>
2020-05-04 16:05:25 +0300

    Placeholder for archive page added

---
+++ src/pages/arkisto.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div class="article">
+      <section class="header">
+        <h1>Arkistop</h1>
+      </section>
+      <section>
+        <g-image src="~/assets/images/keyboard-1.jpg" alt="Näppäimistö" />
+        <div class="content">
+          Not implemented yet.
+        </div>
+      </section>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo () {
+    return {
+      title: 'Arkisto',
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: 'Tälle sivulle on tulossa blogikirjoitusarkisto.'
+        }
+      ]
+    }
+  }
+}
+</script>
