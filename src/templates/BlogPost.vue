5f880a5bef4aba4c0355417123b773d2446557f1
Jarkko Tervonen <jarkko.tervonen@gmail.com>
2021-04-12 16:29:26 +0300

    og tags added

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -16,16 +16,48 @@
 </template>
 
 <script>
+import config from "/gridsome.config.js"
+
 export default {
   metaInfo () {
     return {
       title: this.$page.blogPost.title,
       meta: [
         {
-          key: 'description',
-          name: 'description',
+          key: "description",
+          name: "description",
           content: this.$page.blogPost.description
-        }
+        },
+        {
+          key: "og:url",
+          name: "og:url",
+          content: config.siteUrl + this.$page.blogPost.path
+        },
+        {
+          key: "og:type",
+          name: "og:type",
+          content: "website"
+        },
+        {
+          key: "og:title",
+          name: "og:title",
+          content: this.$page.blogPost.title ? this.$page.blogPost.title : ""
+        },
+        {
+          key: "og:description",
+          name: "og:description",
+          content: this.$page.blogPost.description ? this.$page.blogPost.description : ""
+        },
+        {
+          key: "og:image",
+          name: "og:image",
+          content: this.$page.blogPost.image ? config.siteUrl + this.$page.blogPost.image.src : ""
+        },
+        {
+          key: "twitter:creator",
+          name: "twitter:creator",
+          content: "@jarkkotervonen"
+        },
       ]
     }
   }
@@ -41,6 +73,7 @@ export default {
       content
       image
       image_text
+      path
     }
   }
 </page-query>
